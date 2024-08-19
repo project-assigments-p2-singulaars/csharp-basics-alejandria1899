7828b54e3aeb2c9155eca37f70d57512cb01384f
roouugeer <rogeresteve5@hotmail.com>
2024-08-19 12:28:50 +0200

    done bloques de codigo

---
+++ bloques-de-codigo/Program.cs
@@ -0,0 +1,38 @@
+﻿// bool flag = true;
+// int value = 10;
+// if (flag) 
+// {
+//     Console.WriteLine($"Inside the code block: {value}");
+// }
+
+
+// Console.WriteLine($"Outside the code block : {value}");
+
+// bool flag = true;
+// if (flag)
+//     Console.WriteLine(flag);
+
+// string name = "steve";
+// if (name == "bob") 
+//     Console.WriteLine("Found Bob");
+// else if (name == "steve") 
+//     Console.WriteLine("Found Steve");
+// else 
+//     Console.WriteLine("Found Chuck");
+
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+bool found = false;
+
+foreach (int number in numbers)
+{
+    
+    total += number;
+    if (number == 42)
+    found = true;
+}
+
+if (found) 
+    Console.WriteLine("Set contains 42");
+
+Console.WriteLine($"Total: {total}");
