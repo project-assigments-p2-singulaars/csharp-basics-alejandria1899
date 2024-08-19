c2824a82b3f70bca7624519865221a816ac2dfcb
roouugeer <rogeresteve5@hotmail.com>
2024-08-19 13:13:24 +0200

    done switch-case

---
+++ switch-case/Program.cs
@@ -0,0 +1,95 @@
+﻿// switch (fruit)
+// {
+//     case "apple":
+//         Console.WriteLine($"App will display information for apple.");
+//         break;
+
+//     case "banana":
+//         Console.WriteLine($"App will display information for banana.");
+//         break;
+
+//     case "cherry":
+//         Console.WriteLine($"App will display information for cherry.");
+//         break;
+
+
+// }
+
+// int employeeLevel = 100;
+// string employeeName = "John Smith";
+
+// string title = "";
+
+// switch (employeeLevel) 
+// {
+//     case 100:        
+//     case 200:
+//         title = "Senior Associate";
+//         break;
+//     case 300:
+//         title = "Manager";
+//         break;
+//     case 400:
+//         title = "Senior Manager";
+//         break;
+//     default:
+//         title = "Associate";
+//         break;
+// }
+
+// Console.WriteLine($"{employeeName}, {title}");
+
+string sku = "01-MN-L";
+
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+switch (product[0])
+{
+    case "01":
+        type = "Sweat shirt";
+        break;
+    case "02":
+        type = "T-shirt";
+        break;
+    case "03":
+        type = "Sweat pants";
+        break;
+    default:
+        type = "Other";
+        break;
+}
+
+switch (product[01])
+{
+    case "BL":
+        color = "Black";
+        break;
+    case "MN":
+        color = "Maroon";
+        break;
+    default:
+        color = "White";
+        break;
+}
+
+switch (product[2])
+{
+    case "S":
+        size = "Small";
+        break;
+    case "M":
+        size = "Medium";
+        break;
+    case "L":
+        size = "Large";
+        break;
+    default:
+        size = "One size Fits All";
+        break;
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
